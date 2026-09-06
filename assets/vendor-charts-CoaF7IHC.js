7ac14ed6085c0fbe49eb43555ecdcb9945b92843
Vivek Kumar <vivek@surakshadrishti.ai>
2026-09-06 15:40:32 +0530

    deploy: update with Indian govt registries & area safety predictor

--- assets/vendor-charts-CoaF7IHC.js
+++ assets/vendor-charts-CoaF7IHC.js
@@ -1,4 +1,4 @@
-import{r as gM,c as Gl,g as Ne,a as ne,R as I}from"./vendor-icons-CJ1OO7g0.js";var Dd={exports:{}},Ft={},Ld={exports:{}},Bd={};/**
+import{r as gM,c as Gl,g as Ne,a as ne,R as I}from"./vendor-icons-D8vrg0zE.js";var Dd={exports:{}},Ft={},Ld={exports:{}},Bd={};/**
  * @license React
  * scheduler.production.min.js
  *
