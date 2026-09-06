f461bc7e30dfbaad2ae11d42ce1822c673189bdf
Vivek Kumar <vivek@surakshadrishti.ai>
2026-09-06 16:29:45 +0530

    deploy: show only logged-in user profile in navbar

--- assets/vendor-charts-Bv3d1rBq.js
+++ assets/vendor-charts-Bv3d1rBq.js
@@ -1,4 +1,4 @@
-import{r as gM,c as Gl,g as Ne,a as ne,R as I}from"./vendor-icons-_3Ui3O4C.js";var Dd={exports:{}},Ft={},Ld={exports:{}},Bd={};/**
+import{r as gM,c as Gl,g as Ne,a as ne,R as I}from"./vendor-icons-Cs3wPMhh.js";var Dd={exports:{}},Ft={},Ld={exports:{}},Bd={};/**
  * @license React
  * scheduler.production.min.js
  *
